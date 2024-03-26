f22d312f3c5ce7c2ff1eb4467f42b45dc2199115
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2024-03-26 03:36:30 +0100

    data scraper

---
+++ data-scraper/main.py
@@ -0,0 +1,8 @@
+import scraper
+import dataParser
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    user=input()
+    print(scraper.scrape_user(user))
+
---
+++ data-scraper/scraper.py
@@ -0,0 +1,25 @@
+import httpx
+import json
+
+client = httpx.Client(
+	headers={
+		# this is internal ID of an instegram backend app. It doesn't change often.
+		"x-ig-app-id": "936619743392459",
+		# use browser-like features
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+		"Accept-Language": "en-US,en;q=0.9,ru;q=0.8",
+		"Accept-Encoding": "gzip, deflate, br",
+		"Accept": "*/*",
+	}
+)
+
+
+def scrape_user(username: str):
+	"""Scrape Instagram user's data"""
+	result = client.get(
+		f"https://i.instagram.com/api/v1/users/web_profile_info/?username={username}",
+	)
+	data = json.loads(result.content)
+	formatted_data = json.dumps(data["data"]["user"], indent=4)
+
+	return formatted_data
