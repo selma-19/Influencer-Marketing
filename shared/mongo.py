945cad27fbcec4c44c4bf7589812525d7739a134
chadda <chadha.essid@insat.ucar.tn>
2024-05-01 23:55:00 +0100

    prepare data for LDA

--- shared/mongo.py
+++ shared/mongo.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 client = MongoClient("localhost", 27017)
 db = client.InfluencersMarketing
 collection = db.Influencers
-posts = db.posts_1 #modify this to your mongo post collection's name
+posts = db.posts_8 #modify this to your mongo post collection's name
