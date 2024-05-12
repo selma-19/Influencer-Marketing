da119de6096572e482e9ca4052b5ca96b26387be
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2024-05-12 21:56:38 +0100

    update cleaning script

--- shared/mongo.py
+++ shared/mongo.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 client = MongoClient("localhost", 27017)
 db = client.InfluencersMarketing
 collection = db.NewInfluencers
-posts = db.posts #modify this to your mongo post collection's name
+posts = db.newPosts #modify this to your mongo post collection's name
