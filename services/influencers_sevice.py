a60a24adeb510931af481c2fe0961f9730f74169
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2024-05-13 16:32:49 +0100

    Made the database connection a singleton
    reading from .ini file

--- services/influencers_sevice.py
+++ services/influencers_sevice.py
@@ -1,21 +1,49 @@
-import shared.mongo as mongo
+import configparser
+
+from shared.mongo import MongoConnection
+
+mongo_connection = MongoConnection()
+db = mongo_connection.get_database()
+
+config = configparser.ConfigParser()
+config.read('../config.ini')
+mongo_config = config['MongoDB']  # Access the MongoDB section
+
+posts_collection = db[mongo_config.get("POSTS_COLLECTION", "Posts")]
+influencers_collection = db[mongo_config.get("INFLUENCERS_COLLECTION", "Influencers")]
+
 
 def get_influencers():
-    return mongo.collection.find()
+    try:
+        return list(influencers_collection.find())
+    except Exception as e:
+        print(f"An error occurred while fetching influencers: {e}")
+        return []
 
 
 def get_posts():
-    return mongo.posts.find()
+    try:
+        return list(posts_collection.find())
+    except Exception as e:
+        print(f"An error occurred while fetching posts: {e}")
+        return []
 
 
 def update_influencer(influencer, key, value):
-    filter = {'_id': influencer['_id']}
-    update = {'$set': {key: value}}
-    return mongo.collection.update_one(filter, update)
+    try:
+        filter = {'_id': influencer['_id']}
+        update = {'$set': {key: value}}
+        return influencers_collection.update_one(filter, update)
+    except Exception as e:
+        print(f"An error occurred while updating an influencer: {e}")
+        return None
 
 
 def update_post(post, key, value):
-    filter = {'_id': post['_id']}
-    update = {'$set': {key: value}}
-    return mongo.posts.update_one(filter, update)
-
+    try:
+        filter = {'_id': post['_id']}
+        update = {'$set': {key: value}}
+        return posts_collection.update_one(filter, update)
+    except Exception as e:
+        print(f"An error occurred while updating a post: {e}")
+        return None
--- shared/mongo.py
+++ shared/mongo.py
@@ -1,7 +1,41 @@
-from pymongo import MongoClient
-import re
-client = MongoClient("localhost", 27017)
-db = client.InfluencersMarketing
-collection = db.NewInfluencers
-posts = db.Posts #modify this to your mongo post collection's name
+import configparser
+from typing import Optional
 
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+
+
+class MongoConnection:
+    _instance: Optional['MongoConnection'] = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+
+            config = configparser.ConfigParser()
+            config.read('../config.ini')
+            mongo_config = config['MongoDB']  # Access the MongoDB section
+            cls.host = mongo_config.get("HOST", "localhost")
+            cls.port = int(mongo_config.get("PORT", 27017))
+            cls.database_name = mongo_config.get("DB_NAME", "InfluencersMarketing")
+        try:
+            # Creating client
+            cls.client = MongoClient(cls.host, cls.port, serverSelectionTimeoutMS=5000)
+
+            # Testing the connection
+            cls.client.admin.command('ping')
+
+            cls.database = cls.client[cls.database_name]
+            cls.collection = cls.database[mongo_config.get("INFLUENCERS_COLLECTION")]
+            cls.posts = cls.database[mongo_config.get("POSTS_COLLECTION")]
+
+            print("MongoDB connection established")
+        except ConnectionFailure as e:
+            cls._instance = None
+            print(f"Could not connect to MongoDB: {e}")
+
+        return cls._instance
+
+
+    def get_database(self):
+        return self.database
