829d4a7778bbb51ca84bb46d90cf2d32029e9587
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2024-05-13 02:56:22 +0100

    update

--- shared/mongo.py
+++ shared/mongo.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 client = MongoClient("localhost", 27017)
 db = client.InfluencersMarketing
 collection = db.NewInfluencers
-posts = db.newPosts #modify this to your mongo post collection's name
+posts = db.Posts #modify this to your mongo post collection's name
